7706a37730c92c8834041e12f17edcef0759432d
Diego Alzate <diegoalzate.0207@gmail.com>
2023-04-18 16:00:01 -0500

    REG-5-CRUD, subiendo CRUD tabla resultado

---
+++ Controladores/ControladorResultado.py
@@ -0,0 +1,46 @@
+from Repositorios.RepositorioResultado import RepositorioResultado
+from Repositorios.RepositorioMesa import RepositorioMesa
+from Repositorios.RepositorioCandidato import RepositorioCandidato
+from Repositorios.RepositorioPartido import RepositorioPartido
+from Modelos.Resultado import Resultado
+from Modelos.Mesa import Mesa
+from Modelos.Candidato import Candidato
+from Modelos.Partido import Partido
+
+
+class ControladorResultado():
+    def __init__(self):
+        self.repositorioResultado = RepositorioResultado()
+        self.repositorioMesa = RepositorioMesa()
+        self.repositorioCandidato = RepositorioCandidato()
+        self.repositorioPartido = RepositorioPartido()
+        print("Creando ControladorResultado")
+
+    def index(self):
+        return self.repositorioResultado.findAll()
+
+    def create(self, infoResultado, id_mesa, id_candidato):
+        nuevoResultado = Resultado(infoResultado)
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        nuevoResultado.mesa = laMesa
+        nuevoResultado.candidato = elCandidato
+        return self.repositorioResultado.save(nuevoResultado)
+
+    def show(self, id):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        return elResultado.__dict__
+
+    def update(self, id, infoResultado, id_mesa, id_candidato):
+        elResultado = Resultado(self.repositorioResultado.findById(id))
+        elResultado.numMesa = infoResultado["numero_mesa"]
+        elResultado.idPartido = infoResultado["id_partido"]
+        laMesa = Mesa(self.repositorioMesa.findById(id_mesa))
+        elCandidato = Candidato(self.repositorioCandidato.findById(id_candidato))
+        elResultado.mesa = laMesa
+        elResultado.candidato = elCandidato
+        return self.repositorioResultado.save(elResultado)
+
+    def delete(self, id):
+        return self.repositorioResultado.delete(id)
+
